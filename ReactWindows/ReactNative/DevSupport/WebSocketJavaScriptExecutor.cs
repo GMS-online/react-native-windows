613959f4f885a6d17139b34b2bb40eaa6654be50
Matt Hargett <plaztiksyke@gmail.com>
2018-03-06 18:15:01 -0800

    Fix UWP build. Again, my preference would be to have WebSocketJavaScriptExecutor be Shared.

--- ReactWindows/ReactNative/DevSupport/WebSocketJavaScriptExecutor.cs
+++ ReactWindows/ReactNative/DevSupport/WebSocketJavaScriptExecutor.cs
@@ -10,11 +10,13 @@ using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Networking.Sockets;
 using Windows.Storage.Streams;
+using ReactNative.Modules.DevSupport;
 
 namespace ReactNative.DevSupport
 {
     class WebSocketJavaScriptExecutor : IJavaScriptExecutor
     {
+        private readonly IDeveloperSettings _devSettings;
         private const int ConnectTimeoutMilliseconds = 5000;
         private const int ConnectRetryCount = 3;
 
@@ -28,8 +30,9 @@ namespace ReactNative.DevSupport
         private int _requestId;
         private bool _isDisposed;
 
-        public WebSocketJavaScriptExecutor()
+        public WebSocketJavaScriptExecutor(IDeveloperSettings devSettings)
         {
+            _devSettings = devSettings;
             _webSocket = new MessageWebSocket();
             _webSocket.Control.MessageType = SocketMessageType.Utf8;
             _webSocket.MessageReceived += OnMessageReceived;
